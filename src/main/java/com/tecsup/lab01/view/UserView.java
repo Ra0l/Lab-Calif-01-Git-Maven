c5088acf074b6a1bdff1d2273a0ad5d9d1ff3e9c
Alumno <Alumno@LAB1507-13>
2019-03-21 14:15:08 -0500

    first commit

---
+++ src/main/java/com/tecsup/lab01/view/UserView.java
@@ -0,0 +1,10 @@
+package com.tecsup.lab01.view;
+
+public class UserView {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/como/tecsup/lab01/Lab_Calif_01_Git_Maven_G3/App.java
@@ -0,0 +1,13 @@
+package como.tecsup.lab01.Lab_Calif_01_Git_Maven_G3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
