b34beaa6ed237e68681bb25b3930895a0dafcb41
Jeremy <jeremi.nunez@tecsup.edu.pe>
2019-03-21 15:06:49 -0500

    Cambios view2

--- src/main/java/com/tecsup/lab01/view/UserView.java
+++ src/main/java/com/tecsup/lab01/view/UserView.java
@@ -1,10 +1,17 @@
 package com.tecsup.lab01.view;
 
+import javax.swing.JOptionPane;
+
+import com.tecsup.lab01.controller.UserController;
+
 public class UserView {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			System.out.println("Listo");
+		UserController x = new UserController();
+		String nombre = JOptionPane.showInputDialog(null, "Ingrese nombre: ");
+		String paterno = JOptionPane.showInputDialog(null, "Ingrese paterno: ");
+		int edad = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese edad: "));
+		JOptionPane.showMessageDialog(null, x.obtenerDatos(nombre, paterno, edad));
 	}
 
 }
