4a21e4d6cd980297d73660572f072556f907b441
Alumno <Alumno@LAB1507-13>
2019-03-21 14:58:21 -0500

    cambios controller

--- src/main/java/com/tecsup/lab01/controller/UserController.java
+++ src/main/java/com/tecsup/lab01/controller/UserController.java
@@ -1,5 +1,9 @@
 package com.tecsup.lab01.controller;
+import com.tecsup.lab01.model.User;
 
 public class UserController {
-
+	
+	public String obtenerDatos (String nombre, String paterno, int edad) {
+		return "Nombre completo: " + nombre + " " + paterno + "\nEdad: " + edad;
+	}
 }
